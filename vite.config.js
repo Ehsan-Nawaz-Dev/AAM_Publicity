451611b7a5f0a8fd029498eb186f368b4e9c9def
Malik <redrose4u293@hotmail.com>
2026-07-12 16:02:32 +0500

    Configure api rewrites and proxy target for http://aampower.com in vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://aampower.com',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:4000',
+        target: 'http://aampower.com',
         changeOrigin: true,
       }
     }
